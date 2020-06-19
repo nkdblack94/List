b4331b6599d96570534009d55d996d49c324e947
nkdblack94 <nkdblack1294@gmail.com>
2020-06-19 13:53:04 +0700

    [Thực hành] Triển khai lớp List đơn giản

---
+++ src/test/java/MyListTest.java
@@ -0,0 +1,18 @@
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> integerMyList = new MyList<Integer>();
+        integerMyList.add(1);
+        integerMyList.add(2);
+        integerMyList.add(3);
+        integerMyList.add(3);
+        integerMyList.add(4);
+
+        System.out.println("Element 4: " + integerMyList.get(4));
+        System.out.println("Element 1: " + integerMyList.get(1));
+        System.out.println("Element 2: " + integerMyList.get(2));
+        System.out.println("Element 3: " + integerMyList.get(3));
+
+        integerMyList.get(-1);
+        System.out.println("Element 6: " + integerMyList.get(-1));
+    }
+}
